d9b80a041b1909bfe674ab2779d2f005a920de85
Vince Zhou18 <vincezhou18@gmail.com>
2019-04-30 13:04:02 +0800

    feat:init project

---
+++ src/main/java/top/keepmoving/mysite/controller/DemoController.java
@@ -0,0 +1,20 @@
+package top.keepmoving.mysite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * <p>@Author:Vince
+ * <p>@Date:2019-04-30
+ * <p>@Description:
+ */
+@Controller
+@RequestMapping("/")
+public class DemoController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+}
